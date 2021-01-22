f2ee4acd778eb756cb688b80c580a2e8e9259fc5
Tushar Mistry <mistry.tushar98@gmail.com>
2021-01-22 13:51:07 +0530

    bciqqda

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,11 +1,9 @@
 import BlogComponent from "../../components/BlogComponent";
-import {
-  getBlogByParams,
-  getFooter,
-  getHeader,
-} from "../../helper/query";
+import { getBlogByParams, getFooter, getHeader } from "../../helper/query";
 import Navigation from "../../components/Navigation";
 import Footer from "../../components/footer";
+import Stack from "../../helper/fetchAllEmployee";
+
 function Blog(props) {
   return props.isError ? (
     <h1>Backend server is not responding try again later</h1>
@@ -15,52 +13,73 @@ function Blog(props) {
       <header>
         <Navigation data={props.header} />
       </header>
-      <BlogComponent blog={props.data[0]} />
+      <BlogComponent blog={props.blog} />
       <Footer data={props.footer} />
     </div>
   );
 }
 
-export const getStaticProps = async (context) => {
-  try {
-    let data = await getBlogByParams({ id: context.params.id });
-    data = data[0];
-    let header = await getHeader();
-    header = header[0];
-    let footer = await getFooter();
-    footer = footer[0];
-    return {
-      props: {
-        data: data,
-        header: header,
-        footer: footer,
-      },
-    };
-  } catch (err) {
-    console.log(err);
-    return {
-      props: {
-        isError: true,
-      },
-    };
-  }
-};
+// export const getStaticProps = async (context) => {
+//   try {
+//     let data = await getBlogByParams({ id: context.params.id });
+//     data = data[0];
+//     let header = await getHeader();
+//     header = header[0];
+//     let footer = await getFooter();
+//     footer = footer[0];
+//     return {
+//       props: {
+//         data: data,
+//         header: header,
+//         footer: footer,
+//       },
+//     };
+//   } catch (err) {
+//     console.log(err);
+//     return {
+//       props: {
+//         isError: true,
+//       },
+//     };
+//   }
+// };
 
-export const getStaticPaths = async () => {
-  let data = await getBlogByParams();
-  let paths = [
-    ...data[0].map((blog) => {
-      return {
-        params: {
-          id: `${blog.uid}`,
-        },
-      };
-    }),
-  ];
+// export const getStaticPaths = async () => {
+//   let data = await getBlogByParams();
+//   let paths = [
+//     ...data[0].map((blog) => {
+//       return {
+//         params: {
+//           id: `${blog.uid}`,
+//         },
+//       };
+//     }),
+//   ];
 
+//   return {
+//     paths,
+//     fallback: true,
+//   };
+// };
+
+export const getServerSideProps = async (context) => {
+  const Query = Stack.ContentType("aachal_blog").Query();
+  console.log(context.params.id);
+  let data = await Query.where("uid", `${context.params.id}`)
+    // .includeReference([`${context.params.id}`])
+    .toJSON()
+    .find();
+  // console.log(result[0]);
+  let header = await getHeader();
+  header = header[0];
+  let footer = await getFooter();
+  footer = footer[0];
   return {
-    paths,
-    fallback: true,
+    props: {
+      header,
+      blog: data[0][0],
+      footer,
+    },
   };
 };
 
